2e01cedbf441867b528696582ae4213513af8df6
orange-cors <mango.cors@gmail.com>
2025-11-04 17:30:32 +0700

    FN ver

--- Calculator/script.js
+++ Calculator/script.js
@@ -74,7 +74,7 @@ class Calculator {
             case '+':
                 computation = prev + current;
                 break;
-            case '-':
+            case '–':
                 computation = prev - current;
                 break;
             case '×':
